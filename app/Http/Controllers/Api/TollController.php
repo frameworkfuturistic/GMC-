c6c722919f702ab83237ec1382281bffacf7d66a
frameworkfuturistic <frameworkfuturistic@gmail.com>
2022-08-04 12:00:35 +0530

    New Changes

---
+++ app/Http/Controllers/Api/TollController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Repository\Toll\EloquentTollRepository;
+use Illuminate\Http\Request;
+
+class TollController extends Controller
+{
+    /**
+     *| Created On-07-07-2022
+     *| Created By-Anshu Kumar
+     *| -----------------------------------------------------------------------------------------
+     *| Save Toll and Toll Payment
+     *
+     */
+    protected $eloquentToll;
+    public function __construct(EloquentTollRepository $eloquentToll)
+    {
+        $this->EloquentToll = $eloquentToll;
+    }
+
+    /**
+     * Save Toll Payment (Version 2)
+     */
+    public function tollPayment(Request $request, $id)
+    {
+        return $this->EloquentToll->tollPayment($request, $id);
+    }
+
+    /**
+     * Get Tolls
+     */
+    public function getTollById($id)
+    {
+        return $this->EloquentToll->getTollById($id);
+    }
+
+    /**
+     * Get All Tolls
+     */
+    public function getAllToll()
+    {
+        return $this->EloquentToll->getAllToll();
+    }
+
+    // Get Location By Area
+    public function getTollLocation()
+    {
+        return $this->EloquentToll->getTollLocation();
+    }
+
+    // Get Vendor Details By Ids
+    public function getVendorDetailsByArea(Request $request)
+    {
+        return $this->EloquentToll->getVendorDetailsByArea($request);
+    }
+
+    // Save Toll
+    public function saveToll(Request $request)
+    {
+        return $this->EloquentToll->saveToll($request);
+    }
+
+    // Update Toll
+    public function updateToll(Request $request, $id)
+    {
+        return $this->EloquentToll->updateToll($request, $id);
+    }
+
+    // Get Toll Area List
+    public function getAreaList()
+    {
+        return $this->EloquentToll->getAreaList();
+    }
+
+    // Total Toll Collection
+    public function totalCollection(Request $request)
+    {
+        return $this->EloquentToll->totalCollection($request);
+    }
+}
