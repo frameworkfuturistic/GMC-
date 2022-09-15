b6967537f3c052f5eb52032010910e0913a444bc
Anshu Kumar <anshu8581@gmail.com>
2022-09-15 15:55:21 +0530

    Shop Master Export Excel

--- app/Exports/ShopExport.php
+++ app/Exports/ShopExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Shop;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -13,23 +14,50 @@ class ShopExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        $shop = Shop::select('ShopNo', 'Circle', 'Allottee', 'Rate', 'Arrear', 'Address', 'ContactNo', 'Longitude', 'Latitude')
-            ->get();
-        return $shop;
+        $query = "SELECT 
+                        s.Circle,
+                        s.Market,
+                        s.Allottee,
+                        s.ShopNo,
+                        s.Address,
+                        s.Rate,
+                        s.Arrear,
+                        monthyear(p.PaidFrom) AS Fromm,
+                        monthyear(p.PaidTo) AS Too,
+                        p.PaidTo,
+                        p.Amount,
+                        p.PmtMode,
+                        p.Months,
+                        @b:=case
+                        when @a:=date_format(CURDATE(),'%Y')*12+date_format(CURDATE(),'%m')-p.PaidTo > 0 then @a
+                        ELSE '0'
+                        END AS remaining_month,
+                        (@b*s.Rate)+s.Arrear AS new_demand
+                    FROM shops s
+                    LEFT JOIN shop_payments p ON p.id=s.LastTranID
+                ";
+        $shop = DB::select($query);
+        return collect($shop);
     }
 
     public function headings(): array
     {
         return [
-            'ShopNo',
             'Circle',
+            'Market',
             'Allottee',
+            'ShopNo',
+            'Address',
             'Rate',
             'Arrear',
-            'Address',
-            'ContactNo',
-            'Longitude',
-            'Latitude'
+            'From',
+            'To',
+            'PaidTo',
+            'Amount',
+            'PmtMode',
+            'Months',
+            'RemainingMonth',
+            'NewDemand'
         ];
     }
 }
